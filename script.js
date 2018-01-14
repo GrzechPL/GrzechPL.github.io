87054ae2023e1445fa500d7b745cea219adbd503
GrzechPL <31289461+GrzechPL@users.noreply.github.com>
2018-01-14 16:31:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-  const apiRoot = 'https://localhost:8080/v1/task/';
-  const trelloApiRoot = 'https://localhost:8080/v1/trello/';
+  const apiRoot = 'https://sleepy-eyrie-16788.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://sleepy-eyrie-16788.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
