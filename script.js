104e0bd560bc9fd132ccbeebacb6f6fbd2741223
“GrzechPL <grzech78@o2.pl>
2018-01-15 10:45:39 +0100

    15/01/2018

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-
-  const apiRoot = 'https://localhost:8080/v1/task/';
-  const trelloApiRoot = 'https://localhost:8080/v1/trello/';
+  const apiRoot = 'http://sleepy-eyrie-16788.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'http://sleepy-eyrie-16788.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
